67ba11162760517e4bee037b63b807e44d6059fe
Kyle <me@kyleundefined.dev>
2026-03-19 20:54:21 -0400

    upgrade to astro 6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,12 @@
-import { defineCollection, z } from 'astro:content';
+import { z } from 'astro/zod';
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-    type: 'content',
+    loader: glob({
+        pattern: ['**/*.{md,mdx}', '!**/_*/**', '!**/_*.{md,mdx}'],
+        base: "./src/content/blog"
+    }),
     schema: () => z.object({
         title: z.string(),
         description: z.string(),
@@ -11,7 +16,10 @@ const blog = defineCollection({
 });
 
 const projects = defineCollection({
-    type: 'content',
+    loader: glob({
+        pattern: ['**/*.{md,mdx}', '!**/_*/**', '!**/_*.{md,mdx}'],
+        base: "./src/content/projects"
+    }),
     schema: () => z.object({
         title: z.string(),
         description: z.string(),
